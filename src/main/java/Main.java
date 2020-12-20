91a6535a271b9c77481d5a796fcd6480e9528361
Alessandro Frangiamone <alessandro.frangiamone96@gmail.com>
2020-12-20 11:01:11 +0100

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+package main.java;
+
+import main.java.view.MenuView;
+
+public class Main {
+
+    public static void main(String[] args) {
+        new MenuView();
+    }
+
+}
---
+++ src/main/java/view/MenuView.java
@@ -0,0 +1,15 @@
+package main.java.view;
+
+import javax.swing.*;
+
+public class MenuView extends JFrame {
+    public MenuView(){
+
+        super("Menù");
+        this.setSize(900, 900);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setResizable(false);
+
+        this.setVisible(true);
+    }
+}
